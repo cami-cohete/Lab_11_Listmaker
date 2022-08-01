164c01a83623b89bcced60c634bb054be8aa4161
cami-cohete <esquivca@mail.uc.edu>
2022-07-31 20:36:25 -0400

    Final Commit. Done (:

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,20 @@ public class Main
 
     public static void main(String[] args)
     {
-        boolean done = false;
         final String menu = "A - Add, D - Delete, P - Print, & Q - Quit";
         String cmd = "";
         Scanner in = new Scanner(System.in);
+        String userInput = "";
+        boolean userAddYN = false;
+        boolean userYN = false;
+        boolean done = false;
+        int numToDelete = 0;
+        int numToAdd = 0;
 
-	    do {
-            // display list
-            displayList();
-            
-            // display menu
-            // get menu choice
+        // display list
+        displayList();
+        do {
+            // display menu and Get menu choice
             cmd = SafeInputCopy.getRegExString(in,menu,"[AaDdPpQq]");
             cmd = cmd.toUpperCase();
 
@@ -25,27 +28,53 @@ public class Main
             switch (cmd)
             {
                 case "A":
-                    // Prompt user for list item
-                    // Use your getRangedInt method to get the item number to delete, etc.
-                    // make sure it is not an empty string
-                    // add it to the list
+                    // Prompt user for list item &  make sure it is not an empty string
+                    userInput = SafeInputCopy.getNonZeroLenString(in,"Type the item you wish to add");
+
+                    // Use your getRangedInt method to get the item number to delete, etc. & add it to the list
+                    if (list.size() >= 2) {
+                        userAddYN = SafeInputCopy.getYNConfirm(in, "Do you want to add the item at a specific index?");
+                        if (userAddYN) {
+                            numToAdd = SafeInputCopy.getRangedInt(in, "At what index do you want it added at", 1, list.size());
+                            numToAdd = numToAdd - 1;
+                            list.add(numToAdd, userInput);
+                        }
+                        else
+                        {
+                            list.add(userInput);
+                        }
+                    }
+                    else
+                    {
+                        list.add(userInput);
+                    }
                     break;
+
                 case "D":
-                    // prompt user for the number of the item to delete
-                    // Use your getRangedInt method to get the item number to delete, etc.
+                    // prompt user for the number of the item to delete & Use your getRangedInt method to get the item number to delete, etc.
+                    numToDelete = SafeInputCopy.getRangedInt(in, "Which item do you wish to delete", 1, list.size());
+
                     // Translate the number to an index by subtracting one
+                    numToDelete = numToDelete - 1;
+
                     // remove the item from the list
+                    list.remove(numToDelete);
                     break;
+
                 case "P":
+                    // display list
                     displayList();
                     break;
+
                 case "Q":
-                    // prompt user if they are sure they want to quit
-                    // Use your getYNConfirm method for the quit prompt etc.
-                    System.exit(0);
+                    // prompt user if they are sure they want to quit & Use your getYNConfirm method for the quit prompt etc.
+                    userYN = SafeInputCopy.getYNConfirm(in,"Are you sure you wish to quit?");
+                    if (userYN)
+                    {
+                        done = true;
+                    }
                     break;
             }
-            System.out.println("cmd is " + cmd);
 
          } while (!done);
     }
@@ -58,7 +87,7 @@ public class Main
             {
                 for (int x = 0; x < list.size(); x++)
                 {
-                    System.out.printf("%3d%35s", x + 1, list.get(x));
+                    System.out.printf(" %-3d%-31s\n", x + 1, list.get(x));
                 }
             }
             else
